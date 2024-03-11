9c0d4a44264ac6f600f8108754514b31ff9a3528
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 11:55:28 +0000

    Add celcius to fahrenheit (both ways) functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return parseFloat(((5 / 9) * (temperature - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return parseFloat((temperature * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
