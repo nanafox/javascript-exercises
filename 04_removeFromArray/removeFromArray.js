93e8ac7ae38adcb975aca4a18e027472288b2b5c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 10:47:29 +0000

    Add a function that removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+/**
+ *
+ * @param {Array} array The array of elements
+ * @param  {...any} elementsToRemove The elements to remove from the array.
+ * @returns A new array with the elements removed if they existed in the
+ *          original array.
+ */
+const removeFromArray = function (array, ...elementsToRemove) {
+  const newArray = Array.from(array);
 
+  for (const element of elementsToRemove) {
+    // let's remove every occurrence of the element in the array
+    while (newArray.includes(element)) {
+      const index = newArray.indexOf(element);
+      newArray.splice(index, 1);
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
