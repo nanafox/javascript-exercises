3fe2e8e09d39c00da321132e564f1f27ef1a057e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 11:25:37 +0000

    Add a function that returns the sum of a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start < 0) return 'ERROR';
+  if (typeof (start) !== 'number' || typeof (end) !== 'number') {
+    return 'ERROR';
+  }
 
+  // get the higher end number
+  const n = (start > end) ? start : end;
+
+  return (n * (n + 1)) / 2;
 };
 
 // Do not edit below this line
