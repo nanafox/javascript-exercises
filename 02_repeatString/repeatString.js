4f677c61953b37668136c473843054a2603b0a2a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 04:14:58 +0000

    Add a function that repeats a string, 'n' times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, n) {
+  let string = "";
 
+  // we don't work with negative numbers
+  if (n < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < n; i++) {
+    string += str;
+  }
+
+  return string;
 };
 
 // Do not edit below this line
