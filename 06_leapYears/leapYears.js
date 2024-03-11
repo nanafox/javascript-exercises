4f7e339cc6f16c000813e2052ad28690da6479a5
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 11:37:30 +0000

    Add a function that checks for leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 4 === 0 && year % 100 !== 0) return true;
 
+  if (year % 400 === 0) return true;
+
+  // assume not leap year by default
+  return false;
 };
 
 // Do not edit below this line
