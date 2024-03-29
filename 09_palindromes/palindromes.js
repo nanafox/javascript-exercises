de1b09188963ecc4e7acd0b0efdcc168e9164e52
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-29 14:43:13 +0000

    Add palindromes function with string cleaning logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  if (str === '') return true;
 
+  const tempStr = getCleanString(str);
+
+  for (let i = 0, c = tempStr.length - 1;
+    i < tempStr.length, c >= 0; c--, i++) {
+    if (tempStr[i] !== tempStr[c])
+      return false; // not palindrome when any letter mismatches
+  }
+
+  return true;
+};
+
+const getCleanString = (str) => {
+  const punctuations = ['!', '?', '.', ',', "'"];
+  let tempStr = str.toLowerCase().replaceAll(' ', '');
+
+  for (let c = 0; c < tempStr.length; c++) {
+    if (punctuations.includes(tempStr[c])) {
+      tempStr = tempStr.replace(tempStr[c], '');
+    }
+  }
+  return tempStr;
 };
 
 // Do not edit below this line
