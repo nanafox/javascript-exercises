53e9271aa1081c08d0c04f0ff4d5f8c4e5e51f98
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-30 19:35:26 +0000

    Refactor fibonacci function to handle negative input and optimize performance

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
+  const fibs = [0, 1];
 
+  if (n < 0) return 'OOPS';
+  if (n === 1 || n === 2) return fibs[n - 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibs.push(fibs[i - 1] + fibs[i - 2]);
+  }
+
+  return fibs[n];
 };
 
 // Do not edit below this line
