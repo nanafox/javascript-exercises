37fe9ccb2ef18f122d5e8caf5f555e233593a31a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-01 09:20:35 +0000

    Refactor findTheOldest function to calculate the oldest person based on year of birth and death

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = (peopleObjects) => {
+  let oldest;
+  let initial = 0;
 
+  peopleObjects.forEach(person => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+
+    const age = person.yearOfDeath - person.yearOfBirth;
+    if (age > initial) {
+      initial = age;
+      oldest = person;
+    }
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
